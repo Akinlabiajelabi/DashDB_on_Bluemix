fcf6ac4226acce541deaa53f5b525524c4ba5ea5
rajeevbrahma <rajeevbrahma@hotmail.com>
2016-11-02 17:27:43 +0530

    added return values

--- dashDB_Demo.py
+++ dashDB_Demo.py
@@ -6,9 +6,12 @@ import ibm_db
 from ibm_db import connect
 from ibm_db import active 
 
+import datetime
+
 # importing logging module
 # To log messages
 import logging
+
 
 LOG_FILENAME = "dashDB_DemoLogs.log"
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG,format='%(asctime)s, %(levelname)s, %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -17,9 +20,9 @@ logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG,format='%(asctime)
 class DashDB:
 	def __init__(self):
 		self.connection=None
-		self.userid = ""
-		self.password = ""
-		self.hostname = ""
+		self.userid = "dash100629"
+		self.password = "tQd0KuLmwUeC"
+		self.hostname = "dashdb-entry-yp-dal09-09.services.dal.bluemix.net"
 
 	def dashDBInit(self):
 		self.databaseConnectionInfo = {"Database name":"BLUDB","User ID":self.userid,"Password":self.password,"Host name":self.hostname,"Port number":"50000"}
@@ -43,7 +46,7 @@ class DashDB:
 				else:
 				    hasVcap = False
 				    self.url = 'DATABASE=%s;uid=%s;pwd=%s;hostname=%s;port=%s;' % (self.databaseConnectionInfo["Database name"],self.databaseConnectionInfo["User ID"],self.databaseConnectionInfo["Password"],self.databaseConnectionInfo["Host name"],self.databaseConnectionInfo["Port number"])
-	   
+	   			print self.url
 				self.connection = ibm_db.connect(self.url, '', '')
 				if (active(self.connection)):
 					print self.connection
@@ -55,114 +58,182 @@ class DashDB:
 		return False
 		
 	def connectioncheck_handler(self):
-		
-		logging.info("connection is"+str(active(self.connection)))
-		dbretry = 0
-		if (active(self.connection) == False):
-			while (dbretry<3):
-				self.connection = ibm_db.connect(self.url ,'' ,'')
-				if active(self.connection) == True:
-					dbretry = 3
-				else:
-					if dbretry == 2:
-						raise Exception("db retry Error")
+		try:
+			logging.info("connection is"+str(active(self.connection)))
+			dbretry = 0
+			if (active(self.connection) == False):
+				while (dbretry<3):
+					self.connection = ibm_db.connect(self.url ,'' ,'')
+					if active(self.connection) == True:
+						dbretry = 3
 					else:
-						dbretry+=1
-				
-			logging.info("restarted connection is"+str(active(self.connection)))
-		
+						if dbretry == 2:
+							raise Exception("db retry Error")
+						else:
+							dbretry+=1
+					
+				logging.info("restarted connection is"+str(active(self.connection)))
+		except Exception as e:
+			logging.error("The connectioncheck_handler error is %s"%(e))		
 
-	def dbCreate(self,col1,col2,col3,col4):
+	def dbCreate(self,tablename,col1,col2,col3,col4,col5):
 		self.connectioncheck_handler()
 
-
-		create_query = "CREATE TABLE \""+tablename+"\"(\""+col1+"\" INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1 MINVALUE 1 NO MAXVALUE NO CYCLE NO CACHE ORDER),\""+col2+"\" VARCHAR(30) NOT NULL,\""+col3+"\" VARCHAR(30) NOT NULL,\""+col4+"\" VARCHAR(30) NOT NULL,\""+col5+"\" INT NOT NULL,\""+col6+"\" TIMESTAMP NOT NULL)ORGANIZE BY ROW;"
-	
-		statement = ibm_db.exec_immediate(self.connection, create_query)
-		ibm_db.free_stmt(statement)	
+		try:	
+			create_query = "CREATE TABLE "+tablename+" ("+col1+" INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1 MINVALUE 1 NO MAXVALUE NO CYCLE NO CACHE ORDER),"+col2+" VARCHAR(30) NOT NULL,"+col3+" VARCHAR(30) NOT NULL,"+col4+" VARCHAR(30) NOT NULL,"+col5+" TIMESTAMP NOT NULL, PRIMARY KEY("+col2+","+col3+"))ORGANIZE BY ROW;"
+			statement = ibm_db.exec_immediate(self.connection, create_query)
+			ibm_db.free_stmt(statement)	
 			
-
-
-
-
+		except Exception as e:
+			logging.error("The dbCreate operation error is %s"%(e))
+			return False
+		except:
+			logging.error("The dbCreate operation error is %s"%(ibm_db.stmt_errormsg()))
+			return False
+		return True
+		
 
 	def dbInsert(self,tablename,emailid,password,username,dateofcreation):
 		self.connectioncheck_handler()
 		try:
-			insert_query = "INSERT INTO "+self.DatabaseSchema+".\'"+str(tablename)+"\' VALUES (DEFAULT,\'"+str(emailid)+"\',\'"+str(password)+"\',\'"+str(username)+"\',\'"+str(dateofcreation)+"\')"
+			insert_query = "INSERT INTO "+self.DatabaseSchema+"."+tablename+" VALUES (DEFAULT,\'"+emailid+"\',\'"+password+"\',\'"+username+"\',\'"+str(dateofcreation)+"\')"
 			statement = ibm_db.exec_immediate(self.connection, insert_query)
 			ibm_db.free_stmt(statement)
-			return True	
-	
+				
+		except Exception as e:
+			logging.error("The dbInsert operation error is %s"%(e))
+			return False
 		except:
 			logging.error("The dbInsert operation error is %s"%(ibm_db.stmt_errormsg()))
-
+			return False	
+		return True
 		
-	def dbUpdate(self,tablename,columnName,updatevalue,conditionColumnName,conditionColumnValue):
+	def dbUpdate(self,tablename,columnName,updatevalue,conditionColumnName1,conditionColumnValue1,conditionColumnName2,conditionColumnValue2):
 		self.connectioncheck_handler()
 
 		try:
-			update_query = "UPDATE "+tablename+" SET "+columnName+" = \'"+str(updatevalue)+"\'  WHERE "+conditionColumnName+" = \'"+str(conditionColumnValue)+"\'"
+			update_query = "UPDATE "+tablename+" SET "+columnName+" = \'"+str(updatevalue)+"\'  WHERE "+conditionColumnName1+" = \'"+str(conditionColumnValue1)+"\' AND "+conditionColumnName2+" = \'"+str(conditionColumnValue2)+"\'"
 		
 			statement = ibm_db.exec_immediate(self.connection, update_query)
 			ibm_db.free_stmt(statement)	
-			return True
+			
+		except Exception as e:
+			logging.error("The dbUpdate operation error is %s"%(e))
+			return False
 		except:
 			logging.error("The dbUpdate operation error is %s"%(ibm_db.stmt_errormsg()))	
-		
-	def dbDelete(self,tablename,conditionColumnName,conditionColumnValue):
+			return False
+		return True	
+
+	def dbDelete(self,tablename,conditionColumnName1,conditionColumnValue1,conditionColumnName2,conditionColumnValue2):
 		self.connectioncheck_handler()
 		try:
-			delete_query = "DELETE * FROM "+self.DatabaseSchema+".\'"+str(tablename)+"\'WHERE "+conditionColumnName+" = \'"+str(conditionColumnValue)+"\'"
+			delete_query = "DELETE FROM "+self.DatabaseSchema+"."+tablename+" WHERE "+conditionColumnName1+" = \'"+conditionColumnValue1+"\' AND "+conditionColumnName2+" = \'"+conditionColumnValue2+"\' "
 			statement = ibm_db.exec_immediate(self.connection, delete_query)
 			ibm_db.free_stmt(statement)
-			return True
-		except:
+
+		except Exception as e:
+			logging.error("The dbDelete operation error is %s"%(e))
+			return False
+		except:	
 			logging.error("The dbDelete operation error is %s"%(ibm_db.stmt_errormsg()))	
+			return False
+		return True
+
 	def dbFetch(self,tablename):
 		self.connectioncheck_handler()
 		try:	
-			fetch_query = "SELECT * FROM "+self.DatabaseSchema+".\'"+str(tablename)+"\'"						
+			fetch_query = "SELECT * FROM "+self.DatabaseSchema+"."+tablename+""						
 			statement = ibm_db.exec_immediate(self.connection, fetch_query)
-			dictionary = ibm_db.fetch_assoc(stmt)
+			dictionary = ibm_db.fetch_assoc(statement)
 			data = []
 			while(dictionary!=False):
 				data.append(dictionary)
-				dictionary = ibm_db.fetch_assoc(stmt)
+				dictionary = ibm_db.fetch_assoc(statement)
 			ibm_db.free_stmt(statement)
-			return data
-		except:
+			
+		except Exception as e:
+			logging.error("The dbFetch operation error is %s"%(e))
+			return False
+		except:		
 			logging.error("The dbFetch operation error is %s"%(ibm_db.stmt_errormsg()))	
-
+			return False
+		return data	
 if __name__ == '__main__':
-	db = Dashdb()
+	db = DashDB()
+	db.dashDBInit()
 
 	while True:
-		inpt = raw_input("Enter the value 1-Insert Data 2-Update Data 3-Fetch Data 4-Delete Data")
+		try:
+			inpt = raw_input("\tEnter the value \t\n0-Create table \t\n1-Insert Data \t\n2-Update Data \t\n3-Fetch Data \t\n4-Delete Data\n\t\t")
 
-		if inpt == "1":
-			tablename = "USERTABLE"
-			emailid = raw_input("Enter the emailid:") 
-			password = raw_input("Enter the password:")
-			username = raw_input("Enter the username:")
-			dateofcreation = str(datetime.datetime.now())
-			db.dbInsert(tablename,emailid,password,username,dateofcreation)
+			if inpt == "0":
+				tablename = "USERTABLE"
+				col1 = "ID"
+				col2 = "EMAILID"
+				col3 = "PASSWORD"
+				col4 = "USERNAME"
+				col5 = "DATEOFCREATION"
+				create_retrn = db.dbCreate(tablename,col1,col2,col3,col4,col5)
+				if create_retrn ==True:
+					print "\t\t TABLE CREATED SUCCESSFULLY"
+				else:
+					print "\t\t TABLE CREATE FAILED"	
+			if inpt == "1":
+				tablename = "USERTABLE"
+				emailid = raw_input("Enter the emailid:") 
+				password = raw_input("Enter the password:")
+				username = raw_input("Enter the username:")
+				dateofcreation = str(datetime.datetime.now())
+				insert_return = db.dbInsert(tablename,emailid,password,username,dateofcreation)
+				if insert_return == True:
+					print "\t\tDATA INSERTED SUCCESSFULLY"
+				else:
+					print "\t\tDATA INSERT FAILED"
 
-		if inpt == "2":
-			tablename = "USERTABLE"
-			columnName = raw_input("Enter the columnname:")
-			updatevalue = raw_input("Enter the updatevalue:")
-			conditionColumnName = raw_input("Enter the condtioncolumnname:")
-			conditionColumnValue = raw_input("Enter the condtioncolumnvalue:")
-			db.dbUpdate(tablename,columnName,updatevalue,conditionColumnName,conditionColumnValue)
-		if inpt == "3":
-			tablename = "USERTABLE"
-			data = db.dbFetch(tablename)
-			print data
+			if inpt == "2":
+				tablename = "USERTABLE"
+				print "\t\t YOU HAVE SELECTED TO UPDATE THE DATA IN THE DASHDB TABLE\n \t\t SELECT THE ANY ONE OF THE COLUMNS TO UPDTE FROM FOLLOWING"
+				print "\t\t\t PASSWORD, USERNAME "
+				
+				columnName = raw_input("Enter the columnname to be updated :")
+				updatevalue = raw_input("Enter the updatevalue :")
+				conditionColumnName1 = "EMAILID"
+				conditionColumnValue1 = raw_input("Enter the EMAILID:")
+				conditionColumnName2 = "PASSWORD"
+				conditionColumnValue2 = raw_input("Enter the PASSWORD:")
 
-		if inpt == "4":
-			tablename = "USERTABLE"
-			conditionColumnName = raw_input("Enter the condtioncolumnname:")
-			conditionColumnValue = raw_input("Enter the condtioncolumnvalue:")
-			db.dbDelete(tablename,conditionColumnName,conditionColumnValue)
-			
+				update_return = db.dbUpdate(tablename,columnName,updatevalue,conditionColumnName1,conditionColumnValue1,conditionColumnName2,conditionColumnValue2)
+				if update_return == True:
+					print "\t\tDATA UPDATED SUCCESSFULLY"
+				else:	
+					print "\t\tDATA UPDATE FAILED"
+
+			if inpt == "3":
+				print "\n\t\t\t DATA FETCH OPERATION"
+				tablename = "USERTABLE"
+				data = db.dbFetch(tablename)
+				if data != None:
+					print "\t\t DATA FETCHED SUCCESSFULLY"
+					print data
+				else:
+					print "\t\t DATA FETCH FAILED"	
+			if inpt == "4":
+				print "\n\t\t\t DATA DELETION OPERATION"
+				
+				tablename = "USERTABLE"
+				conditionColumnName1 = "EMAILID"
+				conditionColumnValue1 = raw_input("Enter the EMAILID:")
+				conditionColumnName2 = "PASSWORD"
+				conditionColumnValue2 = raw_input("Enter the PASSWORD:")
+				
+
+				del_return = db.dbDelete(tablename,conditionColumnName1,conditionColumnValue1,conditionColumnName2,conditionColumnValue2)
+				print del_return
+				if del_return == True:
+					print "\t\t DATA DELETED SUCCESSFULLY"
+				else:
+					print "\t\t DATA DELETE FAILED"	
+		except KeyboardInterrupt:
+			print "\n\t\t\t oops!!! PRESSED ctr+c"
+			sys.exit()			
